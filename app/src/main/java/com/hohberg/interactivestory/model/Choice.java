78430bfb6d4a974e79dd7587e27c50362e4b79f9
stevenhohberg <hohbergsteven@gmail.com>
2016-09-26 11:59:11 +0100

    Initial commit

---
+++ app/src/main/java/com/hohberg/interactivestory/model/Choice.java
@@ -0,0 +1,28 @@
+package com.hohberg.interactivestory.model;
+
+
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
